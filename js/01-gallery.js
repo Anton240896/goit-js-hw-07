6d25ddb5eac520f7efdf040a166705d483897bd3
Anton240896 <antalipov24081111111@gmail.com>
2023-07-27 23:25:33 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 
 const galleryElem = document.querySelector('.gallery');
 const galleryItems = document.createElement('galleryItems')
 
-// const instance = basicLightbox.create(`
-//     <h1>Dynamic Content</h1>
-//     <p>You can set the content of the lightbox with JS.</p>
-// `);
-
-// console.log(instance);
-// instance.show();
 
 galleryElem.insertAdjacentElement('beforeend', galleryItems);
 galleryElem.addEventListener('click',onClick);
 
 function onClick(galleryItems) {
+    return galleryItems
+    .map(({  preview , original , description }) => {
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}">
+        </a></li>`
+    })
     
+
 }
+
+
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`);
+
+console.log(instance);
+instance.show();
