dfcc9416e9364f001a5ea0bcdceafd0f4794dda2
Anton240896 <antalipov24081111111@gmail.com>
2023-07-27 18:58:59 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryElem = document.querySelector('.gallery');
+const galleryItems = document.createElement('galleryItems')
+
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`);
+
+// console.log(instance);
+// instance.show();
+
+galleryElem.insertAdjacentElement('beforeend', galleryItems);
+galleryElem.addEventListener('click',onClick);
+
+function onClick(galleryItems) {
+    
+}
