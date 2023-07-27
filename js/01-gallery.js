d6c8808831bdaa8eb19e2edd2493e29bfea09ffe
Anton240896 <antalipov24081111111@gmail.com>
2023-07-27 21:53:26 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,10 +7,10 @@ console.log(galleryItems);
 const galleryElem = document.querySelector('.gallery');
 const galleryItems = document.createElement('galleryItems')
 
-const instance = basicLightbox.create(`
-    <h1>Dynamic Content</h1>
-    <p>You can set the content of the lightbox with JS.</p>
-`);
+// const instance = basicLightbox.create(`
+//     <h1>Dynamic Content</h1>
+//     <p>You can set the content of the lightbox with JS.</p>
+// `);
 
 // console.log(instance);
 // instance.show();
