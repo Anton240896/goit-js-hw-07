3148afcae786433f5895bd9fe0a2c44cd13947b3
Anton240896 <antalipov24081111111@gmail.com>
2023-07-30 22:27:08 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,33 +1,49 @@
-// import { galleryItems } from './gallery-items.js';
-// // Change code below this line
 
-// console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+//////////////////////////////////////////////////////////////////
+
+                    // displaying elements in the browser
+                    
+  const galleryElement = document.querySelector('.gallery');
+
+ 
+function fooGallery(galeryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    });
+}
+galleryElement.insertAdjacentHTML('beforeend', fooGallery(galleryItems).join(''));
+galleryElement.addEventListener('click', onClick);
 
 
-const galleryElem = document.querySelector('.gallery');
-const galleryItems = document.createElement('galleryItems')
+                  //Implementation of delegation at ul.gallery and open modal window
 
-
-galleryElem.insertAdjacentElement('beforeend', galleryItems);
-galleryElem.addEventListener('click',onClick);
-
-function onClick(galleryItems) {
-    return galleryItems
-    .map(({  preview , original , description }) => {
-      return  `<li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}">
-        </a></li>`
-    })
-    .join('')
-
+function onClick(evt) {
+if (evt.target.nodeName !== "IMG") {
+return;
+  }
+evt.preventDefault();
+  const originalImg = evt.target.dataset.source;
+  
+   
+   const instance = basicLightbox.create(`<img src="${originalImg}">`);
+   instance.show();
 }
 
 
-const instance = basicLightbox.create(`
-    <h1>Dynamic Content</h1>
-    <p>You can set the content of the lightbox with JS.</p>
-`);
 
-console.log(instance);
-instance.show();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryElement = document.querySelector('.gallery');
+const galleryElem = document.querySelector('.gallery__image');
+    // displaying elements in the browser
+        
+
+    function fooGallery(galeryItems) {
+      return galleryItems
+        .map(({ preview, original, description }) => {
+          return `
+            <li class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  alt="${description}"
+                />
+              </a>
+            </li>
+          `;
+        });
+    }
+    galleryElement.insertAdjacentHTML('beforeend', fooGallery(galleryItems).join(''));
+
+
+                  //Implementation of delegation at ul.gallery and open modal window
+
+   
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionClass: "",
+    captionDelay: 250,
+  });
+
